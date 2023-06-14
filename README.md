e610297770fbe045555ae1853b865d237723d784
Sibren <sibrenvansetten@hotmail.nl>
2023-06-14 09:21:17 +0000

    Added force deletion, customizable check period and updated documentation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # S3-Deleter
 Docker image that automatically deletes old files from S3 buckets. Useful for unofficial S3 servers that don't support lifecycle policies or object expiration.
-Images older then the specified amount of days will be deleted every 12 hours.
+Images older then the specified amount of days will be deleted.
 
 ## Usage
 The image is available on the GitHub Container Registry:
@@ -10,13 +10,23 @@ ghcr.io/siebsie23/s3-deleter:latest
 
 To run the image you must provide the following environment variables:
 ```yaml
+HOUR_PERIOD: 24
 SERVER_URL: The URL of the S3 server
 ACCESS_KEY: The access key for the S3 server
 SECRET_KEY: The secret key for the S3 server
 REGION: The region of the S3 server
 DELETE_AFTER_DAYS: The number of days after which files should be deleted
 BUCKETS: A comma-separated list of buckets to delete files from
+FORCE_DELETE: true
 ```
+
+### Variable explenations
+
+`SERVER_URL`: In case you use a different S3 server then the official Amazon one, you can specify it in this variable. Leaving it empty will use the default amazon servers.
+
+`HOUR_PERIOD`: This variable decides how often the script should check for old files. In the example above the script will run every 24 hours.
+
+`FORCE_DELETE`: If the force_delete variable is set to 'true' the script will delete files with the 'BypassGovernanceRetention' set to true. More information about this can be found at [boto3.amazonaws.com](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/delete_object.html#:~:text=User%20Guide.-,BypassGovernanceRetention,-(boolean)%20%E2%80%93%20Indicates)
 
 You can also run the image from a docker-compose file, an example of which is provided below:
 ```yaml
@@ -25,12 +35,14 @@ services:
   autodelete:
     image: ghcr.io/siebsie23/s3-deleter:latest
     environment:
+      HOUR_PERIOD: 24
       SERVER_URL: https://eu2.contabostorage.com
       ACCESS_KEY: 1234567890
       SECRET_KEY: 1234567890
       REGION: eu-central-1
       DELETE_AFTER_DAYS: 10
       BUCKETS: bucket1,bucket2,bucket3
+      FORCE_DELETE: true
 ```
 
 A full list of regions can be found here:
@@ -44,7 +56,6 @@ docker build -t s3-deleter .
 
 ## Future Plans
 - Add support for multiple servers
-- Customize how often the script should check for old files
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import schedule
+""" This is the main script, it will be run by the Docker
+container and will delete the files from the AWS S3 server """
 import time
-import boto3
-from datetime import datetime, timedelta
 import os
+from datetime import datetime, timedelta
+import schedule
+import boto3
 
 
 print("Starting AWS S3 file deletion scheduler")
@@ -10,34 +12,45 @@ print("Starting AWS S3 file deletion scheduler")
 # Load the environment variables
 
 # Get the AWS S3 server info and a list of buckets from the environment variables
-url = os.environ.get('SERVER_URL')
-buckets = os.environ.get('BUCKETS').split(',')
+url = os.environ.get("SERVER_URL")
+buckets = os.environ.get("BUCKETS").split(",")
 
 # Get the AWS S3 access key and secret key from the environment variables
-access_key = os.environ.get('ACCESS_KEY')
-secret_key = os.environ.get('SECRET_KEY')
+access_key = os.environ.get("ACCESS_KEY")
+secret_key = os.environ.get("SECRET_KEY")
 
 # Get the AWS S3 region from the environment variables
-region = os.environ.get('REGION')
+region = os.environ.get("REGION")
 
 # Get the amount of days to keep the files from the environment variables
-days = int(os.environ.get('DELETE_AFTER_DAYS'))
+days = int(os.environ.get("DELETE_AFTER_DAYS"))
+
+# Get if the files should be force deleted from the environment variables
+force_delete = os.environ.get("FORCE_DELETE")
+
+# Get the hour period to run the job from the environment variables
+hour_period = int(os.environ.get("HOUR_PERIOD"))
 
 
-def job():
+def job() -> None:
+    """ This is the main job that will be run by the scheduler,
+    it loops trough the buckets and deletes the files that are older than the
+    specified amount of days from the environment variables """
     # Create an S3 client using the access key, secret key, and region
-    print("Connecting to {} AWS S3 server".format(url))
-    s3_client = boto3.client('s3', endpoint_url=url, aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
+    print(f"Connecting to {url} AWS S3 server")
+    s3_client = boto3.client(
+        "s3", endpoint_url=url, aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region
+    )
 
     # Loop through the buckets
     for bucket in buckets:
-        print("Checking {} bucket".format(bucket))
+        print(f"Checking {bucket} bucket")
         # List all objects in the bucket
         response = s3_client.list_objects_v2(Bucket=bucket)
-        if 'Contents' in response:
-            for obj in response['Contents']:
+        if "Contents" in response:
+            for obj in response["Contents"]:
                 # Get the last modified timestamp of the object
-                last_modified = obj['LastModified'].replace(tzinfo=None)
+                last_modified = obj["LastModified"].replace(tzinfo=None)
 
                 # Calculate the difference between the current date and the last modified date
                 difference = datetime.now() - last_modified
@@ -45,18 +58,23 @@ def job():
                 # Check if the difference is greater than the specified number of days
                 if difference > timedelta(days=days):
                     # Delete the object
-                    print("Deleting {} from AWS S3".format(obj['Key']))
-                    s3_client.delete_object(Bucket=bucket, Key=obj['Key'])
+                    print(f"Deleting {obj['Key']} from AWS S3")
+                    # Check if the file should be force deleted
+                    if force_delete == "true":
+                        # Force delete the file, skipping the versioning
+                        s3_client.delete_object(Bucket=bucket, Key=obj["Key"], BypassGovernanceRetention=True)
+                    else:
+                        s3_client.delete_object(Bucket=bucket, Key=obj["Key"])
 
-    print("Files older than {} days have been deleted from AWS S3".format(days))
+    print(f"Files older than {days} days have been deleted from AWS S3")
 
 
-schedule.every(12).hours.do(job)
+schedule.every(hour_period).hours.do(job)
 
 print("Running the job now before starting the scheduler")
 job()
 
-print("Scheduler started! Deleting files older than {} days from AWS S3 every 12 hours.".format(days))
+print(f"Scheduler started! Deleting files older than {days} days from AWS S3 every 12 hours.")
 
 while True:
     schedule.run_pending()
